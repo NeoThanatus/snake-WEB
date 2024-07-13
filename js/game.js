88ca8e76764c1649c7e3049b25bca26963849d05
NeoThanatus <inushirodesu@gmail.com>
2024-07-12 21:51:55 -0400

    TP1- Ranulfo

--- js/game.js
+++ js/game.js
@@ -4,31 +4,71 @@
 
   let board;
   let snake;
-
+  let apple;
+  let score;
+  setInterval(run, 1000 / FPS)
   function init() {
+    score = new Score();
     board = new Board(SIZE);
     snake = new Snake([[4, 4], [4, 5], [4, 6]])
-    setInterval(run, 1000 / FPS)
+    apple = new Apple(snake.body)
+    
+  }
+
+  function restart() {
+    board.element.remove();
+    score.element.remove();
+    snake = null;
+    apple = null;
+    board = null;
+    FPS = 10;
+    
+    init()
   }
 
   window.addEventListener("keydown", (e) => {
     switch (e.key) {
       case "ArrowUp":
+        if(snake.direction === 2) return;
         snake.changeDirection(0)
         break;
       case "ArrowRight":
+        if(snake.direction === 3) return;
         snake.changeDirection(1)
         break;
       case "ArrowDown":
+        if(snake.direction === 0) return;
         snake.changeDirection(2)
         break;
       case "ArrowLeft":
+        if(snake.direction === 1) return;
         snake.changeDirection(3)
+        break;
+      case "s":
+        restart();
+       
         break;
       default:
         break;
     }
   })
+  class Score{
+    constructor(){
+      this.scoreValue = 0;
+      this.element = document.createElement("div");
+      this.element.setAttribute("id", "score");
+      document.body.appendChild(this.element);
+      this.element.innerHTML = `Score: 0`
+    }
+    updateScore(){
+      console.log(this.scoreValue.toString().padStart(4, "0")); 
+      this.element.innerHTML = `Score: ${this.scoreValue.toString().padStart(4, "0")}`
+    }
+    addPoints(points){
+      this.scoreValue += points;
+      this.updateScore();
+  }}
+
 
   class Board {
     constructor(size) {
@@ -46,6 +86,38 @@
       }
     }
   }
+
+  class Apple{
+    constructor(snakeBody){
+      this.color = "black"
+      this.position = [Math.floor(Math.random() * SIZE), Math.floor(Math.random() * SIZE)]
+      this.snakeBody = snakeBody;
+      this.updatePosition()
+      
+    }
+    chooseNewPosition(snakeBody){
+      let newPosition;
+      let isPositionOccupied;
+      do{
+        newPosition = [Math.floor(Math.random() * SIZE), Math.floor(Math.random() * SIZE)];
+        isPositionOccupied = snakeBody.some(field => field[0] === newPosition[0] && field[1] === newPosition[1]);
+      } while(isPositionOccupied);
+      
+      if(this.color === "red"){
+        score.addPoints(20);
+        updateScore(score.scoreValue);
+      } else {
+        score.addPoints(10);
+        updateScore(score.scoreValue);
+      }
+      this.color = Math.random() < 1/2 ? "black" : "red";
+      return newPosition;
+    }
+    updatePosition(){
+      document.querySelector(`#board tr:nth-child(${this.position[0]}) td:nth-child(${this.position[1]})`).style.backgroundColor = this.color
+    };
+  }
+
 
   class Snake {
     constructor(body) {
@@ -73,6 +145,7 @@
         default:
           break;
       }
+      
       this.body.push(newHead)
       const oldTail = this.body.shift()
       document.querySelector(`#board tr:nth-child(${newHead[0]}) td:nth-child(${newHead[1]})`).style.backgroundColor = this.color
@@ -81,10 +154,37 @@
     changeDirection(direction) {
       this.direction = direction
     }
+    detectCollision() {
+      const head = this.body[this.body.length - 1];
+      const isAppleEaten = head[0] === apple.position[0] && head[1] === apple.position[1];
+      const isSnakeEatingItself = this.body.some((field, index) => index < this.body.length - 1 && field[0] === head[0] && field[1] === head[1]);
+      
+      if (isAppleEaten) {
+        apple.position = apple.chooseNewPosition(this.body);
+        apple.updatePosition();
+        const tail = this.body[0];
+        const secondLast = this.body[1];
+        let newTail = [tail[0] * 2 - secondLast[0], tail[1] * 2 - secondLast[1]];
+        FPS += 1;
+        this.body.unshift(newTail);
+      }
+      if (isSnakeEatingItself ) {
+        gameOver();
+    }}
   }
-
+  function updateScore(score) {
+    const scoreElement = document.getElementById('score');
+    scoreElement.innerText = `Score: ${score}`;
+  }
   function run() {
     snake.walk()
+    snake.detectCollision()
+    //setInterval(this.run, 1000 / FPS)
+  }
+  function gameOver(){
+    alert("Game Over!\nPressione S para reiniciar")
   }
   init()
+  
+
 })()
